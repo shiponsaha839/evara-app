c77bfa445435f2f31fac7b32a751d68666267db2
SHIPON SAHA <100799000+shiponsaha839@users.noreply.github.com>
2024-06-29 17:44:24 +0600

    Delete resources directory

--- resources/views/website/includes/style.blade.php
+++
@@ -1,4 +0,0 @@
-
-<link rel="shortcut icon" type="image/x-icon" href="{{asset('/')}}website/assets/imgs/theme/favicon.svg">
-<!-- Template CSS -->
-<link rel="stylesheet" href="{{asset('/')}}website/assets/css/maind134.css?v=3.4">
